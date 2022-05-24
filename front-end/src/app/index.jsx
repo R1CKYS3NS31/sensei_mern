0670c70a73a344365686f3f5e2c3e4b3d7523c28
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-05-24 08:41:15 +0300

    routes commit

--- front-end/src/app/index.jsx
+++ front-end/src/app/index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import NavBar from '../components/NavBar'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from "../components/NavBar";
+import { MoviesList, MoviesInsert, MovieUpdate } from "../pages";
 
 function App() {
   return (
     <Router>
-      <NavBar/>
+      <NavBar />
+      <Switch>
+        <Route path="/movies/list" exact component={MoviesList} />
+        <Route path="/movies/create" exact component={MoviesInsert} />
+        <Route path="/movies/update/:id" exact component={MovieUpdate} />
+      </Switch>
     </Router>
   );
 }
