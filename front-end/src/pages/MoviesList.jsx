8da8f926f06aab98ba9289d37ea90ba5c70a0392
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-05-24 08:45:32 +0300

    MovieList commit

---
+++ front-end/src/pages/MoviesList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class MoviesList extends Component {
+  render() {
+    return (
+      <div>
+          <p>
+              In this page you'll see the list of movies
+          </p>
+      </div>
+    )
+  }
+}
