b49b138e60e67b3a337dd2185792fea1d4a0eff3
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-05-24 08:44:29 +0300

    MovieInsert commit

---
+++ front-end/src/pages/MoviesInsert.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class MoviesInsert extends Component {
+  render() {
+    return (
+        <div>
+            <p>
+                In this pae you'll see the form to add a movie
+            </p>
+        </div>
+    )
+  }
+}
